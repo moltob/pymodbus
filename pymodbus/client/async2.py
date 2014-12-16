5c80161b9286a581a210a70dd9ee35051b92808d
Mike Pagel <mike@mpagel.de>
2014-12-16 09:25:01 +0100

    Tested and updated example code, removed non-ported classes.

--- pymodbus/client/async2.py
+++ pymodbus/client/async2.py
@@ -4,33 +4,23 @@ Implementation of a Modbus Client Using asyncio
 
 Example run::
 
-    from twisted.internet import reactor, protocol
-    from pymodbus.client.async import ModbusClientProtocol
+    import asyncio
+    from pymodbus.client.async2 import ModbusClientProtocol
 
-    def printResult(result):
-        print "Result: %d" % result.bits[0]
 
-    def process(client):
-        result = client.write_coil(1, True)
-        result.addCallback(printResult)
-        reactor.callLater(1, reactor.stop)
-
-    defer = protocol.ClientCreator(reactor, ModbusClientProtocol
-            ).connectTCP("localhost", 502)
-    defer.addCallback(process)
-
-Another example::
-
-    from twisted.internet import reactor
-    from pymodbus.client.async import ModbusClientFactory
-
+    @asyncio.coroutine
     def process():
-        factory = reactor.connectTCP("localhost", 502, ModbusClientFactory())
-        reactor.stop()
+        transport_, protocol_ = yield from asyncio.get_event_loop().create_connection(ModbusClientProtocol, "localhost", 502)
 
-    if __name__ == "__main__":
-       reactor.callLater(1, process)
-       reactor.run()
+        result = yield from protocol_.read_coils(1)
+        print("Result: %d" % result.bits[0])
+
+        transport_.close()
+
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(process())
+    loop.close()
 """
 from pymodbus.factory import ClientDecoder
 from pymodbus.exceptions import ConnectionException
@@ -125,7 +115,7 @@ class ModbusClientProtocol(Protocol, ModbusClientMixin):
         for the current request.
 
         :param tid: The transaction identifier for this response
-        :returns: A defer linked to the latest request
+        :returns: A future linked to the latest request
         '''
         f = Future()
         if self._connected:
@@ -135,90 +125,24 @@ class ModbusClientProtocol(Protocol, ModbusClientMixin):
             f.set_exception(ConnectionException('Client is not connected'))
         return f
 
-        #----------------------------------------------------------------------#
-        # Extra Functions
-        #----------------------------------------------------------------------#
-        #if send_failed:
-        #       if self.retry > 0:
-        #               deferLater(clock, self.delay, send, message)
-        #               self.retry -= 1
 
+# Not yet ported:
+# class ModbusUdpClientProtocol
+# class ModbusClientFactory
 
-#---------------------------------------------------------------------------#
-# Not Connected Client Protocol
-#---------------------------------------------------------------------------#
-class ModbusUdpClientProtocol(DatagramProtocol, ModbusClientMixin):
-    '''
-    This represents the base modbus client protocol.  All the application
-    layer code is deferred to a higher level wrapper.
-    '''
-
-    def __init__(self, framer=None):
-        ''' Initializes the framer module
-
-        :param framer: The framer to use for the protocol
-        '''
-        self.framer = framer or ModbusSocketFramer(ClientDecoder())
-        if isinstance(self.framer, ModbusSocketFramer):
-            self.transaction = DictTransactionManager(self)
-        else:
-            self.transaction = FifoTransactionManager(self)
-
-    def datagram_received(self, data, addr):
-        ''' Get response, check for valid message, decode result
-
-        :param data: The data returned from the server
-        :param params: The host parameters sending the datagram
-        '''
-        _logger.debug("Datagram from: %s:%d" % addr)
-        self.framer.processIncomingPacket(data, self._handleResponse)
-
-    def execute(self, request):
-        ''' Starts the producer to send the next request to
-        consumer.write(Frame(request))
-        '''
-        request.transaction_id = self.transaction.getNextTID()
-        packet = self.framer.buildPacket(request)
-        self.transport.write(packet)
-        return self._buildResponse(request.transaction_id)
-
-    def _handleResponse(self, reply):
-        ''' Handle the processed response and link to correct deferred
-
-        :param reply: The reply to process
-        '''
-        if reply is not None:
-            tid = reply.transaction_id
-            handler = self.transaction.getTransaction(tid)
-            if handler:
-                handler.callback(reply)
-            else:
-                _logger.debug("Unrequested message: " + str(reply))
-
-    def _buildResponse(self, tid):
-        ''' Helper method to return a deferred response
-        for the current request.
-
-        :param tid: The transaction identifier for this response
-        :returns: A defer linked to the latest request
-        '''
-        d = defer.Deferred()
-        self.transaction.addTransaction(d, tid)
-        return d
-
-
-#---------------------------------------------------------------------------#
-# Client Factories
-#---------------------------------------------------------------------------#
-class ModbusClientFactory(protocol.ReconnectingClientFactory):
-    ''' Simple client protocol factory '''
-
-    protocol = ModbusClientProtocol
+# asyncio does not have a reconnecting client factory built-in, but even
+# Twisted devs don't like the current approach, see:
+# https://groups.google.com/forum/#!topic/python-tulip/9lIABK73zAc
+#
+# A similar approach was implemented at higher level in Tulip example
+# code, see:
+# https://github.com/leetreveil/tulip/blob/master/examples/cacheclt.py
 
 #---------------------------------------------------------------------------#
 # Exported symbols
 #---------------------------------------------------------------------------#
 __all__ = [
-    "ModbusClientProtocol", "ModbusUdpClientProtocol",
-    "ModbusClientFactory",
+    "ModbusClientProtocol",
+    # "ModbusUdpClientProtocol",
+    # "ModbusClientFactory",
 ]
